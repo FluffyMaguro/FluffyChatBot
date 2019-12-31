3720417139e2a764d88729d06b7a39e07fa4467c
Maguro <59398682+FluffyMaguro@users.noreply.github.com>
2019-12-31 20:04:50 +0100

    Add files via upload

---
+++ FluffyChatBot.py
@@ -0,0 +1,271 @@
+import socket
+import string
+import time
+import threading
+import cv2
+import numpy as np
+import pyautogui
+import os
+import pandas as pd
+
+CHANNEL = "" #channel name where bot operates (all lowercase)
+NICK = "" #bot name (all lowercase)
+PASS = "" #twitch API key for bot
+HOST = "irc.twitch.tv"
+PORT = 6667
+findingActivated = True
+postCurrent = False
+mutatorsFound = False
+
+def openSocket():
+    s = socket.socket()
+    s.connect((HOST, PORT))
+    s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+    s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+    s.send("JOIN #{}\r\n".format(CHANNEL).encode("utf-8"))
+    return s
+
+def joinRoom(s):
+    readbuffer_join = "".encode()
+    Loading = True
+    while Loading:
+        readbuffer_join = s.recv(1024)
+        readbuffer_join = readbuffer_join.decode()
+        temp = readbuffer_join.split("\n")
+        readbuffer_join = readbuffer_join.encode()
+        readbuffer_join = temp.pop()
+
+        for line in temp:
+            Loading = loadingComplete(line)
+
+    print("VeryFluffyBot has joined the chat")
+    sendMessage(s,'/color green')
+
+def loadingComplete(line):
+    if ("End of /NAMES list" in line):
+        return False
+    else:
+        return True
+
+def getUser(line):
+    separate = line.split(":", 2)
+    user = separate[1].split("!", 1)[0]
+    return user
+
+def getMessage(line):
+    separate = line.split(":", 2)
+    message = separate[2]
+    return message
+
+def console(line):
+    if "PRIVMSG" in line:
+        return False
+    else:
+        return True    
+
+def sendMessage(s, message):
+    messageTemp = "PRIVMSG #" + CHANNEL + " :" + message
+    print("Sent: " + message)
+    try:
+        s.send("{}\r\n".format(messageTemp).encode("utf-8"))
+    except BrokenPipeError:
+        s = openSocket()
+        s.send("{}\r\n".format(messageTemp).encode("utf-8"))
+
+def pingsAndMessages():
+    global findingActivated
+    global postCurrent
+    chatColor = 'green'
+
+    while True:
+        try:
+            readbuffer = s.recv(1024)
+            readbuffer = readbuffer.decode()
+            temp = readbuffer.split("\n")
+            readbuffer = readbuffer.encode()
+            readbuffer = temp.pop()
+        except:
+            temp = ""
+
+        for line in temp:
+            if line == "":
+                break
+            
+            if "PING" in line and console(line):
+                msgg = "PONG :tmi.twitch.tv\r\n".encode()
+                s.send(msgg)
+                print(msgg)
+                break
+
+            #Commands
+            user = getUser(line) 
+            message = getMessage(line)
+            first_word = message.split()[0]
+            print(user + ": " + message)
+
+            if "@VeryFluffyBot" in line and not(console(line)):
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'*little fluffy bot likes tuna*')
+
+            if "!tuna" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'*munches* thunk you! ')
+
+            if "!stop" == first_word and user == CHANNEL: 
+                findingActivated = False
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'No catching little mutator things, fine! *yawns*')
+
+            if "!start" == first_word and user == CHANNEL:
+                postCurrent = False 
+                findingActivated = True
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'Find them all, got it! *sniffs*')
+
+            if "!website" == first_word or "!site" == first_word or "!blog" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'www.maguro.one')
+
+            if "!twitter" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'twitter.com/FluffyMaguro')
+
+            if "!mutators" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'www.maguro.one/p/mutators.html')
+
+            if "!commands" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                sendMessage(s,'!website, !twitter, !mutators, !current')
+
+            if "!current" == first_word:
+                sendMessage(s,'/color ' + chatColor)
+                if findingActivated == False or mutatorsFound == False:
+                    sendMessage(s,'cannot do that right now')
+                else:
+                    sendMessage(s,'let me see...')
+                    postCurrent = True
+
+        time.sleep(1)
+
+def getBrutalPlus (diff):
+        level = 'undefined'
+        if 0 < diff < 4:
+            level = 'Too easy for Brutal+'
+        if 4 <= diff <= 6:
+            level = 'Brutal+1'
+        if 7 <= diff <= 8:
+            level = 'Brutal+2'
+        if 9 <= diff <= 10:
+            level = 'Brutal+3'
+        if 11 <= diff <= 12:
+            level = 'Brutal+4'
+        if 13 <= diff <= 14:
+            level = 'Brutal+4.5'
+        if 15 <= diff <= 16:
+            level = 'Brutal+5'
+        if 17 <= diff <= 18:
+            level = 'Brutal+5.5'
+        if 19 <= diff <= 20:
+            level = 'Brutal+6'
+        if  diff > 20:
+            level = 'Harder than any Brutal+'
+        return level
+
+
+def FindMutators():
+    global postCurrent
+    global mutatorsFound
+
+    MutatorDescriptions = {"Concussive Attacks": "Player units are slowed by all enemy attacks.", "Eminent Domain": "Enemies gain control of your structures after destroying them.", "Evasive Maneuvers": "Enemy units teleport a short distance away upon taking damage.", "Life Leech": "Enemy units and structures steal life or shields whenever they do damage.", "Orbital Strike": "Enemy Orbital Strikes are periodically fired throughout the map.", "Photon Overload": "All enemy structures attack nearby hostile units.", "Shortsighted": "Player units and structures have reduced vision range.", "Temporal Field": "Enemy Temporal Fields are periodically deployed throughout the map.", "Time Warp": "Enemy Time Warps are periodically deployed throughout the map.", "Alien Incubation": "All enemy units spawn Broodlings upon death.", "Barrier": "Enemy units and structures gain a temporary shield upon the first time they take damage.", "Darkness": "Previously explored areas remain blacked out on the minimap while outside of player vision.", "Hardened Will": "Enemy Heroic units reduce all incoming damage to a maximum of 10 when any non-heroic enemy unit is near them.", "Inspiration": "Enemy Heroic units increase the attack speed and armor of all enemies within a small range. ", "Laser Drill": "An enemy Laser Drill constantly attacks player units within enemy vision.", "Long Range": "Enemy units and structures have increased weapon and vision range.", "Mineral Shields": "Mineral clusters at player bases are periodically encased in a shield which must be destroyed for gathering to continue.", "Moment of Silence": "When a Heroic enemy dies, all player units around it will reflect on their transgressions, leaving them unable to attack or use abilities.", "Purifier Beam": "An enemy Purifier Beam moves across the map toward nearby player units.", "Scorched Earth": "Enemy units set the terrain on fire upon death.", "Speed Freaks": "Enemy units have increased movement speed.", "Twister": "Tornadoes move across the map, damaging and knocking back player units in their path.", "Walking Infested": "Enemy units spawn Infested Terran upon death in numbers according to the unit's life.", "Aggressive Deployment": "Additional enemy units are periodically deployed onto the battlefield.", "Diffusion": "Damage dealt to enemies is split evenly across all nearby units, including your own.", "Double-Edged": "Your units also receive all the damage they deal, but they are healed over time.", "Fatal Attraction": "When enemy units and structures die, any of your nearby units are pulled to their location.", "Fear": "Player units will occasionally stop attacking and run around in fear upon taking damage.", "Going Nuclear": "Nukes are launched at random throughout the map.", "Lava Burst": "Lava periodically bursts from the ground at random locations and deals damage to player air and ground units.", "Missile Command": "Endless missile bombardments target your structures and must be shot down throughout the mission.", "Outbreak": "Enemy Infested Terrans spawn continuously around the map.", "Self Destruction": "Enemy units explode and deal damage to nearby player units upon death.", "We Move Unseen": "All enemy units are permanently cloaked.", "Mag-nificent": "Mag Mines are deployed throughout the map at the start of the mission.", "Blizzard": "Storm clouds move across the map, damaging and freezing player units in their path.", "Avenger": "Enemy units gain increased attack speed, movement speed, armor, life, and life-regeneration when nearby enemy units die.", "Micro Transactions": "Giving commands to your units costs resources based on the unit's cost.", "Mutually Assured Destruction": "Enemy Hybrid units detonate a Nuke upon death.", "Power Overwhelming": "All enemy units have energy and use random abilities.", "Slim Pickings": "Player worker units gather resources at a reduced rate, but resource pickups spawn throughout the map.", "Void Reanimators": "Void Reanimators wander the battlefield, bringing your enemies back to life.", "Kill Bots": "Offensive robots of a mysterious origin have been unleashed on the Koprulu sector, intent on destruction. Through cunning engineering, they are invincible until their pre-programmed kill counter has been filled. After that occurs, they will shut down. But can you survive for that long?", "Minesweeper": "Groups of Widow Mines and Spider Mines are buried throughout the battlefield.", "Black Death": "Some enemy units carry a plague that deals damage over time and spreads to other nearby units. The plague spreads to your units when the enemy unit is killed.", "Just Die!": "Enemy units are automatically revived upon death.", "Polarity": "Each enemy unit is immune to either your units or your ally's units.", "Transmutation": "Enemy units have a chance to transform into more powerful units whenever they deal damage.", "Propagators": "Shapeless lifeforms creep toward your base, transforming all of the units and structures they touch into copies of themselves.", "Heroes from the Storm": "Attack waves will be joined by heroes of increasing power.", "Void Rifts": "Void Rifts periodically appear in random locations and spawn enemy units until destroyed."}
+    MutatorDiffScore = {"Concussive Attacks": "1", "Eminent Domain": "1", "Evasive Maneuvers": "1", "Life Leech": "1", "Orbital Strike": "1", "Photon Overload": "1", "Shortsighted": "1", "Temporal Field": "1", "Time Warp": "1", "Alien Incubation": "2", "Barrier": "2", "Darkness": "2", "Hardened Will": "2", "Inspiration": "2", "Laser Drill": "2", "Long Range": "2", "Mineral Shields": "2", "Moment of Silence": "2", "Purifier Beam": "2", "Scorched Earth": "2", "Speed Freaks": "2", "Twister": "2", "Walking Infested": "2", "Aggressive Deployment": "3", "Diffusion": "3", "Double-Edged": "3", "Fatal Attraction": "3", "Fear": "3", "Going Nuclear": "3", "Lava Burst": "3", "Missile Command": "3", "Outbreak": "3", "Self Destruction": "3", "We Move Unseen": "3", "Mag-nificent": "4", "Blizzard": "4", "Avenger": "5", "Micro Transactions": "5", "Mutually Assured Destruction": "5", "Power Overwhelming": "5", "Slim Pickings": "5", "Void Reanimators": "5", "Kill Bots": "6", "Minesweeper": "6", "Black Death": "7", "Just Die!": "7", "Polarity": "7", "Transmutation": "7", "Propagators": "8", "Heroes from the Storm": "10", "Void Rifts": "10"}
+
+    PATH = 'Mutator Icons'
+    INTERVAL = 3 #seconds
+    PreviousMutators = []  
+    colors = ['Red', 'Blue']
+    currentColor = 0  
+
+    while True:
+
+        if findingActivated == False: #skip if the function is deactivated via chat command (temporarily)
+            time.sleep(INTERVAL)
+            print('//mutator find disabled')
+            continue
+
+        MutatorDF = pd.DataFrame(columns=['Mutator', 'Description', 'Y','X','Max_val'])
+        NewMutators = []
+        a = 0
+        FewMutators = False #these prevent doing more work than necessary, if it's either small or big, the checks only those later
+        ManyMutators = False
+        entries = os.scandir(PATH) #for some reason this needs to be rescaned
+        
+        img = pyautogui.screenshot(region=(1810,380, 110, 480))
+        img_rgb = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+        for entry in entries:
+            if entry.is_file() and entry.name.endswith('.png'):
+
+                if len(entry.name.split('_')) > 1 and FewMutators: #tells whether to load small or not
+                    continue
+                if len(entry.name.split('_')) == 1 and ManyMutators:
+                    continue
+
+                template = cv2.imread(PATH +'/'+ entry.name,1)
+
+                res = cv2.matchTemplate(img_rgb,template,cv2.TM_CCOEFF_NORMED)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+                threshold = 0.9
+                loc = np.where( res >= threshold) #array with values where res > threshold
+                        
+                if np.count_nonzero(loc)>0:
+                    mutatorsFound = True
+                    if ManyMutators == False and FewMutators == False:
+                        if len(entry.name.split('_')) > 1: #was it a small of big one?
+                            ManyMutators = True
+                        else:
+                            FewMutators = True
+
+                    NewMutators.append(entry.name.split('.')[0]) #add to pd frames to sort later by position (x,y)
+                    MutatorDF.loc[a] = [entry.name.split('.')[0].split('_')[0]]+[MutatorDescriptions[entry.name.split('.')[0].split('_')[0]]]+[round(max_loc[1]/10,0)]+[round(max_loc[0]/10,0)]+[round(max_val,3)]
+                    a += 1
+
+        MutatorsNotChanged = len(set(PreviousMutators) & set(NewMutators)) >= len(NewMutators)  
+
+        if MutatorsNotChanged and postCurrent == False:
+            pass
+        else:
+            postCurrent = False
+            PreviousMutators = NewMutators
+            
+            if not(MutatorsNotChanged): #sort & save only if mutations changed, and not when using !current command
+                SortedMutatorDF = MutatorDF.sort_values(by=['Y','X'], ascending=True).reset_index()
+                MutatorDF['Mutator'].to_csv('MutatorLog.csv', mode='a', header=False, sep ='\t')
+
+            currentColor += 1
+            if currentColor >= len(colors): #loop back if max
+                currentColor = 0               
+
+            MutationDifficulty = 0
+            Message = '/color ' + colors[currentColor]
+            sendMessage(s,Message)
+
+            for index, row in SortedMutatorDF.iterrows():
+                print(index, row['Mutator'],' ', row['Max_val'])
+                Message = row['Mutator'] + ' ('+ MutatorDiffScore[row['Mutator']] +') - ' + row['Description'] #'/me : '
+                sendMessage(s,Message)
+                MutationDifficulty += int(MutatorDiffScore[row['Mutator']])
+
+            sendMessage(s,'Total difficulty score: ' + str(MutationDifficulty) +' ('+getBrutalPlus(MutationDifficulty)+')')
+
+        time.sleep(INTERVAL)
+
+#start the bot
+if (__name__ == "__main__"):
+    s = openSocket()
+    joinRoom(s)
+    t1 = threading.Thread(target = FindMutators)
+    t1.start()
+    t2 = threading.Thread(target = pingsAndMessages)
+    t2.start()
+    
